9ffade12adcfc9407e578ee24149f603493b2548
B4rn3y <robert.luebke96@freenet.de>
2018-03-17 22:09:40 +0100

    Quest TEST

--- @Valor/addons/data/main/config/Quest_config.hpp
+++ @Valor/addons/data/main/config/Quest_config.hpp
@@ -6,13 +6,29 @@ class Quest_config {
 				0, //unique quest queue ID
 				0,  // unique Quest ID for Quest queue
 				"Offspring", // Header for Quest in Dialog
-				"Blah Blah Plane Blah Egg blah blah bring it to me you fuck idiot", // This is the Description for the Quest, u can use structued text elemts here
-				"Get a Raptor Egg and bring it to me", // Description short
-				"""valor_egg"" in uniformItems player || ""valor_egg"" in vestItems player || ""valor_egg"" in backpackItems player", // boolean code if quest can be submited
-				"player removeitem ""valor_egg""", // code to do when submitted
+				"A Cargo Plane with deadly mutated raptors on it crashed somewhere. The Raptors should have survived this crash, they are very territorial so they probably stay somewhere near the Plane. Can you go and find these raptors and get me an egg of them?", // This is the Description for the Quest, u can use structued text elemts here
+				"Find a Raptor Egg and bring it to me", // Description short
+				"""valor_Raptor_Egg"" in uniformItems player || ""valor_Raptor_Egg"" in vestItems player || ""valor_Raptor_Egg"" in backpackItems player", // boolean code if quest can be submited
+				"player removeitem ""valor_Raptor_Egg""", // code to do when submitted
 				"\UIs\textures\quest\quest_pictures\Quest_0_0_1.jpg", // Path to first Picture
 				"\UIs\textures\quest\quest_pictures\Quest_0_0_2.jpg", // Path to second Picture
-				"You are a good boy" // Text to show when Quest is submitted
+				"Thanks for the egg, here is your reward" // Text to show when Quest is submitted
+
+
+
+
+			},
+			{
+				0, //unique quest queue ID
+				1,  // unique Quest ID for Quest queue
+				"Test", // Header for Quest in Dialog
+				"Barney got a big dick!", // This is the Description for the Quest, u can use structued text elemts here
+				"Do IT", // Description short
+				"true", // boolean code if quest can be submited
+				"hint ""test"";", // code to do when submitted
+				"\UIs\textures\quest\quest_pictures\Quest_0_0_1.jpg", // Path to first Picture
+				"\UIs\textures\quest\quest_pictures\Quest_0_0_2.jpg", // Path to second Picture
+				"Thanks dude" // Text to show when Quest is submitted
 
 
 
