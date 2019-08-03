53f49f7ac704daa6387d822d9084cca4452fad85
Bloodwyn <bloodwyn1715@gmail.com>
2019-08-03 21:24:51 +0200

    drawbridge

--- @Valor/addons/valor_basebuild/config.cpp
+++ @Valor/addons/valor_basebuild/config.cpp
@@ -14,8 +14,8 @@ class CfgPatches
 };
 class CfgVehicles
 {
-	class Items_base_F;
-	class valor_drawBridge: Items_base_F
+	class House_Small_F;
+	class valor_drawBridge: House_Small_F
 	{
 		scope=2;
 		author="Bloodwyn";
@@ -33,11 +33,11 @@ class CfgVehicles
 				source="user";
 				initPhase=0;
 				animPeriod=6;
-				sound="";
+				sound="RoadGateDoors";
 			};
 		};
 	};
-	class valor_baseFloor40: Items_base_F
+	class valor_baseFloor40: House_Small_F
 	{
 		scope=2;
 		author="Bloodwyn";
@@ -48,22 +48,22 @@ class CfgVehicles
 		icon="iconObject";
 		mapSize=1;
 	};
-	class valor_baseFloor45: valor_baseFloor40
+	class valor_baseFloor45: House_Small_F
 	{
 		model="\valor_basebuild\models\stronghold_base_45m.p3d";
 		displayName="Base Floor 45m";
 	};
-	class valor_baseFloor50: valor_baseFloor45
+	class valor_baseFloor50: House_Small_F
 	{
 		model="\valor_basebuild\models\stronghold_base_50m.p3d";
 		displayName="Base Floor 50m";
 	};
-	class valor_baseFloor55: valor_baseFloor45
+	class valor_baseFloor55: House_Small_F
 	{
 		model="\valor_basebuild\models\stronghold_base_55m.p3d";
 		displayName="Base Floor 55m";
 	};
-	class valor_baseFloor60: valor_baseFloor45
+	class valor_baseFloor60: House_Small_F
 	{
 		model="\valor_basebuild\models\stronghold_base_60m.p3d";
 		displayName="Base Floor 60m";
